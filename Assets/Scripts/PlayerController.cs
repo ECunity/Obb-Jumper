8f3dd50679422d58e21ad26f129b57d19efb2955
ECunity <evan.corbett@meritknights.com>
2022-04-05 13:57:14 -0600

    Whoops, it all broke
    I moved over to my PC at home and it seems some stuff didn't carry over correctly, because the entire hierarchy was reset. I had to basically redo everything.
    
    - Remade idle animation
    - Added tilemap and the tile sprites that go with it
    - Created PlayerController, though it has yet to have anything written in it

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame. FixedUpdate is used for physics (Rigidbody2D)
+    void FixedUpdate()
+    {
+        
+    }
+}
